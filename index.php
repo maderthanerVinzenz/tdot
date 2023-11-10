9f7080830d3f40a2f74708039ebc0b1a0c35164e
Ukhaalag Munkherdene <uhaalag.kherlen@htlwy.at>
2023-11-10 09:52:43 +0100

    update index.php

--- index.php
+++ index.php
@@ -25,39 +25,44 @@
         $plz = $_POST['Pzahl'];
         if(isset($_POST['CheckSEW'])){
             $fachID1=1;
+            $fachName = "SEW";
         }
         if(isset($_POST['CheckNWT'])){
             $fachID2=2;
+            $fachName = "NWT";
         }
         if(isset($_POST['CheckINSY'])){
             $fachID3=3;
+            $fachName = "INSY";
         }
+        $fachID = 1;
+        //$fachID = $fachID1 + "," + $fachID2 + "," + $fachID3;
+        $persID = 1;
+        $umfrageID = 1;
 
-        $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-        VALUES ('John', 'Doe', 'john@example.com');";
-        $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
-        VALUES ('Mary', 'Moe', 'mary@example.com');";
-        $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
-        VALUES ('Julie', 'Dooley', 'julie@example.com')";
-
-        $persID1 = 1;
+        $persID = 1;
 
         $conn = new mysqli($host, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
         if($fachID == 1){
-            $sql1 = "Insert Into t_umfrage (TST, FachID, PersID, PLZ) Values($trueTimestamp, $fachID, $persID, $plz)";
+            $sql1 = "Insert Into t_umfrage (TST, FachID, PersID) Values($trueTimestamp, $fachID, $persID)";
+            $sql1 += "\n Insert Into t_person (P_ID, PLZ) Values($persID, $plz)";
+            $sql1 += "\n Insert Into t_fach (Fa_ID, Name) Values($fachID, $fachName)";
             $conn->query($sql1);
         }
         if($fachID== 2){
             $sql2 = "Insert Into t_umfrage (TST, FachID, PersID, PLZ) Values($trueTimestamp, $fachID, $persID, $plz)";
+            $sql2 += "\n Insert Into t_person (P_ID, PLZ) Values($persID, $plz)";
+            $sql2 += "\n Insert Into t_fach (Fa_ID, Name) Values($fachID, $fachName)";
             $conn->query($sql2);
         }
         if($fachID== 3){
             $sql3= "Insert Into t_umfrage (TST, FachID, PersID, PLZ) Values($trueTimestamp, $fachID, $persID, $plz)";
+            $sql3 += "\n Insert Into t_person (P_ID, PLZ) Values($persID, $plz)";
+            $sql3 += "\n Insert Into t_fach (Fa_ID, Name) Values($fachID, $fachName)";
             $conn->query($sql3);
         }
-        
     }
 ?>
