aaa45faf474ef1dea41d71572b89e89e83f6bfa2
Tanja Kerschner <tanja.kerschner@htlwy.at>
2023-11-22 17:39:09 +0100

    fragenEinlesen

--- PHP/fragenEinlesen.php
+++ PHP/fragenEinlesen.php
@@ -1,63 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checkboxen zählen</title>
-</head>
-<body>
-    <form action="test.php" method="post">
-        <?php
-        // Beispiel: Erstellen von fünf Checkboxen in einer Schleife
-        
+<?php
 
-        $host = 'localhost'; // replace with your database host
-        $username = 'root'; // replace with your database username
-        $password = ''; // replace with your database password
-        $dbname = 'd_Fragen'; // replace with your database name
-        // Create connection
-        echo($host);
-        echo($username);
-        echo($password);
-        echo($dbname);
-        $conn = mysqli_connect($host, $username, $password, $dbname);
+$host = 'localhost'; // replace with your database host
+$username = 'root'; // replace with your database username
+$password = ''; // replace with your database password
+$dbname = 'd_Fragen'; // replace with your database name
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-        
 
-        $sql="Select FachID From t_Fachdef Where Fach == 'SEW'"
-        $id = $conn->query($sql);
 
-        $sql1 = "Select Count(Bez) From t_FachFrageDef Where FachID== $id";
-        $count = $conn->query($sql1);
+$sql="Select FachID From t_fachdef Where Fach = 'SEW';";
+$result = $conn->query($sql);
 
-        
-        for ($i = 1; $i <= count; $i++) {
-            $sql="  Select FragenID From t_FachFragenDef Where FachID == '$id' AND FachID == $i;"
-            $frageid = $conn->query($sql); 
-            if($frageid!=null){
-                $sql="Select Bez From t_Fragendef Where FragenID == '$frageid'"
-                $frage = $conn->query($sql);           
-                if($frage!= null){
-                    echo '<input type="checkbox" name="checkbox' . $i . '" value="1"> ' $frage '<br>';
+if ($result) {
+    // Fetch the result row as an associative array
+    $row = $result->fetch_assoc();
+
+    if ($row) {
+        // Retrieve the value of FachID
+        $id = $row['FachID'];
+        $sql1 = "Select Count(FragenID) From t_FachFrageDef;";
+        $countresult = $conn->query($sql1);
+
+        if($countresult){
+            $countrow = $countresult->fetch_assoc();
+            if($countrow)
+            {
+                $count = $countrow['Count(FragenID)'];
+                
+
+                for ($i = 1; $i <= $count; $i++) {
+                    $sql = "SELECT FragenID FROM t_FachFrageDef WHERE FachID = '$id'";
+                    $frageidresult = $conn->query($sql);
+                
+                    if ($frageidresult) {
+                        $frageid = -1;
+                        $row = $frageidresult->fetch_assoc();
+                        
+                        if ($row) {
+                            $frageid = $row['FragenID'];
+                            
+                            $sql = "SELECT Bez FROM t_fragedef WHERE FragenID = '$i'";
+                            $frageresult = $conn->query($sql);
+                
+                            // Check if the second query was successful
+                            if ($frageresult !== false) {
+                                // Check if there are rows in the result set
+                                if ($frageresult->num_rows > 0) {
+                                    while ($frage = $frageresult->fetch_assoc()) {
+                                        echo '<p class="questions hidden"><input type="checkbox" name="checkbox' . $i . '" id="check1" class="checkB">' . $frage['Bez'] . '</p><p></p>';
+                                    }
+                                } else {
+                                    echo "No rows found for FragenID: $frageid";
+                                }
+                            } else {
+                                echo "Error executing the second query: " . $conn->error;
+                            }
+                        }
+                    } else {
+                        echo "Error executing the first query: " . $conn->error;
+                    }
                 }
             }
-            
+
+
+
         }
 
-        $sql="Select FachID From t_Fachdef Where Fach == 'NWT'"
-        for ($i = 1; $i <= count; $i++) {
-            $sql2 = "Select Bez from t_Fragedef Where FragenID == $i";
-            $frage = $conn->query($sql2);
 
-            echo '<input type="checkbox" name="checkbox' . $i . '" value="1"> ' $frage '<br>';
-        }
-        ?>
-        <input name='submit' type="submit" value="Submit">
-    </form>
-</body>
-</html>
+    }
+}
+
+
+
+
+?>
